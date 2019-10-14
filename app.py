314da286efe577260da02f66b80e9938efbe312f
Rakshit Deshpande <rakshitdeshpande07@gmail.com>
2019-10-14 15:50:26 +0530

    updated app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 
 global user
 
+file = open("password","r")
+string = file.read()
+client = pymongo.MongoClient(string)
+db = client.test
+
+
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -18,17 +24,10 @@ def notFound(e):
 def user_login():
     if request.method=="GET":
         return render_template("login.html")
-    elif request.method == "POST":
-        print("POST came")
-        file = open("password","r")
-        string = file.read()
-        client = pymongo.MongoClient(string)
-        db = client.test
-        x = db.test_collection.find({"name":request.form["username"]})
-        # print(x[0]["password"])
+    elif request.method == "POST" :
+        x = db.test_collection.find({"name":request.form["name"]})
         if x[0]["password"] == request.form["password"]:
-            print("HELLO")
-            session['user'] = request.form["username"]
+            session['user'] = request.form["name"]
             return redirect("/skmanager")
         else:
             return redirect("/user_login")
@@ -49,12 +48,10 @@ def signup():
          blood_group = request.form['Blood Group']
          valid_till = request.form['valid_till']
          password = request.form['password']
-         file = open("password","r")
-         string = file.read()
-         client = pymongo.MongoClient(string)
-         db = client.test
          cred = {"name":name,"email":email,"gender":gender,"DLNo":DLNo,"dob":dob,"blood_group":blood_group,"valid_till":valid_till,"password":password}
          db.test_collection.insert(cred)
+         session['user'] = request.form["name"]
+         user = request.form["name"]
          return redirect("/skmanager")
     else:
         return render_template("/signup.html")
@@ -64,29 +61,17 @@ def signup():
 @app.route('/skmanager',methods=["POST","GET"])
 def skmanager():
     if request.method == 'GET':
-        file = open("password","r")
-        string = file.read()
-        client = pymongo.MongoClient(string)
-        db = client.test
         data = db.test_collection.find({"name":session[user]})
         return render_template('sk_manager.html',data = data)
 
 @app.route('/delete')
 def delete():
-    file = open("password","r")
-    string = file.read()
-    client = pymongo.MongoClient(string)
-    db = client.test
     db.test_collection.delete_many({})
     return render_template("sucess.html")
 
 @app.route('/dashboard',methods = ['GET','POST'])
 def dashboard():
     if request.method == 'GET':
-        file = open("password","r")
-        string = file.read()
-        client = pymongo.MongoClient(string)
-        db = client.test
         data = db.test_collection.find({},{"_id":0})
         return render_template('dashboard.html',data = data)
 
@@ -124,10 +109,8 @@ def code():
     if request.method == 'POST':
         codeDetails = request.form
         code = codeDetails['code']
-        file = open("password","r")
-        string = file.read()
-        client = pymongo.MongoClient(string)
-        db = client.test
+        db.test_collection.find({})
+        return "code"
         
     
 
